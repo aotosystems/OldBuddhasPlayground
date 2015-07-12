b9528a4d4561e0d63f7442bcb65815ea48a3fd75
Jon Kurisu <jon.kurisu@aotosystems.com>
2015-07-12 10:40:48 -0700

    Initial Commit

---
+++ BuddhasPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuddhasPlayground
+//
+//  Created by Jon Kurisu on 7/12/15.
+//  Copyright © 2015 AOTO Systems, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
